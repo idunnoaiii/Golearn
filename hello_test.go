2ea7258cb836af9ba58decf5256aa56b022a1bbc
Nguyễn Quang Thiện <84729328+idunnoaiii@users.noreply.github.com>
2021-10-14 01:36:39 +0000

    UPDATE refactor code

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,32 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, world"
-
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
+    
+    assertCorrectMessage := func(t testing.TB, got, want string) {
+        //Helper function help report the line number in function which 
+        //we want tracking instead of this helper fucntion
+        t.Helper()
+        if got != want {
+            t.Errorf("got %q want %q", got, want)
+        }
     }
+
+    t.Run("saying hello to people", func(t *testing.T){
+        got := Hello("Chris", "")
+        want := "Hello, Chris"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("saying hello world when string is empty", func(t *testing.T){
+        got := Hello("", "")
+        want := "Hello, world"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Spanish", func(t *testing.T){
+        got := Hello("Elodie", "Spanish") 
+        want := "Hola, Elodie"
+        assertCorrectMessage(t, got, want)
+    })
+
 }
